c13bcd82f65657e7db3eec1b71db65998c5502a5
Gustavo Melo <gcastromelo94@gmail.com>
2016-04-27 00:25:47 +0000

    nomenclatura

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-console.log('seila');
-console.log('venho trabalhando');
+module.exports = function(opts) {
+  var implicit = (opts && opts.implicit == false) ? false : true;
+
+  return function(style){
+    style.include(__dirname);
+    if (implicit) { style.import('senzo-stylus-components'); }
+  }
+
+}
